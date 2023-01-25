422e5e8aa1b0acc1173025d67dacfe7f2bd0e7a7
Sergey <smtishkar@mail.ru>
2023-01-25 13:49:14 +0300

    3. Забираю переменную из массива

--- lesson2.java
+++ lesson2.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import javax.lang.model.element.Element;
 
 /**
  * lesson2
@@ -15,45 +16,55 @@ public class lesson2 {
         String path = "Input.txt";
         File file = new File(path);
         Scanner scanner = new Scanner(file);
-        // String line = scanner.nextLine();
-        // String[] words = line.split(" ");
 
-        ArrayList<String> data = new ArrayList<String>() ;
-        while (scanner.hasNextLine()){
+        ArrayList<String> data = new ArrayList<String>();
+        while (scanner.hasNextLine()) {
             data.add(scanner.nextLine());
         }
 
-        System.out.println(data);
-        String[] simpleArray = data.toArray(new String[]{});
-        System.out.println(Arrays.toString(simpleArray));
+        // System.out.println(data);
+        String[] simpleArray = data.toArray(new String[] {});
+        // System.out.println(Arrays.toString(simpleArray));
         scanner.close();
 
         String str1 = simpleArray[0];
-        String [] arrtemp1 = str1.split(" = ");
-        for (String ss1: arrtemp1 ){
-            System.out.println(ss1);
+        String[] arrtemp1 = str1.split(" = ");
+        for (String ss1 : arrtemp1) {
+            // System.out.println(ss1);
         }
-        System.out.println(Arrays.toString(arrtemp1));
 
         String str2 = simpleArray[1];
-        String [] arrtemp2 = str2.split(" = ");
-        for (String ss2: arrtemp2 ){
-            System.out.println(ss2);
+        String[] arrtemp2 = str2.split(" = ");
+        for (String ss2 : arrtemp2) {
+            // System.out.println(ss2);
         }
         System.out.println(Arrays.toString(arrtemp1));
         System.out.println(Arrays.toString(arrtemp2));
 
 
+        String z1 = null;
+        String z2 = null;
+
+        for (int i = 0; i < arrtemp2.length; i++) {   
+            if (arrtemp1[0] == "a") {
+                z1 = arrtemp1[1];
+                z2 = arrtemp2[1];
+            } else {
+                z1 = arrtemp2[1];
+                z2 = arrtemp1[1];
+            }
+            // System.out.println(z1);
+        }
+        System.out.println(z1);
+        System.out.println(z2);
+
         // String path = "Input.txt";
         // File file = new File(path);
         // Scanner scanner = new Scanner(file);
         // while (scanner.hasNextLine()){
-        //     System.out.println(scanner.nextLine());
+        // System.out.println(scanner.nextLine());
         // }
         // scanner.close();
-
-
-
 
         double a = 2;
         double b = 3;
@@ -66,9 +77,8 @@ public class lesson2 {
             writer.write(s);
             writer.write(System.lineSeparator());
             // System.out.println(s);
-        }
-        catch(IOException e) {
-            System.out.println(e.getMessage());        
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 
@@ -77,7 +87,5 @@ public class lesson2 {
         return result;
 
     }
-
-    
 
 }
