af6490a5f3883e4d0acc5e3c59a1961a7619fc59
Sergey <smtishkar@mail.ru>
2023-01-25 17:57:46 +0300

    7. все перевел в методы

--- lesson2.java
+++ lesson2.java
@@ -1,18 +1,27 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import javax.lang.model.element.Element;
-
 /**
- * lesson2
+ * Реализовать функцию возведения числа а в степень b. a, b ∈ Z. Сводя количество выполняемых действий к минимуму
  */
 public class lesson2 {
 
     public static void main(String[] args) throws IOException {
+       
+        int [] getDataArr = getData(args);
+        System.out.println(getDataArr[0] + " в степени " + getDataArr[1] + " равен = " + calc(getDataArr));
+        double result = calc(getDataArr);
+        writeData(result,getDataArr);
+
+    }
+
+    public static int [] getData (String[] args) throws FileNotFoundException {
+        int [] finalarr = new int[2];
         String path = "Input.txt";
         File file = new File(path);
         Scanner scanner = new Scanner(file);
@@ -21,107 +30,56 @@ public class lesson2 {
         while (scanner.hasNextLine()) {
             data.add(scanner.nextLine());
         }
-
-        // System.out.println(data);
         String[] simpleArray = data.toArray(new String[] {});
-        // System.out.println(Arrays.toString(simpleArray));
         scanner.close();
 
         String str1 = simpleArray[0];
         String[] arrtemp1 = str1.split(" = ");
-        // for (String ss1 : arrtemp1) {
-        // System.out.println(ss1);
-        // }
 
         String str2 = simpleArray[1];
         String[] arrtemp2 = str2.split(" = ");
-        // for (String ss2 : arrtemp2) {
-        // System.out.println(ss2);
-        // }
-        System.out.println(Arrays.toString(arrtemp1));
-        // System.out.println(Arrays.toString(arrtemp2));
 
         String a1 = null;
         String b1 = null;
 
-        // for (int i = 0; i < arrtemp1.length; i++) {
-        // if (arrtemp1[0] == "b") {
-        // a1 = arrtemp1[1];
-        // b1 = arrtemp2[1];
-        // }
-        // else {
-        // a1 = arrtemp1[1];
-        // b1 = arrtemp2[1];
-        // }
-        // }
-
-
-        // for (int i = 0; i < arrtemp2.length; i++) {
-        //     System.out.println(arrtemp2[i]);
-        // }
-
-
-        // if (arrtemp1[0] == "a") {
-        //     System.out.println("a тута");
-        // }
-
-        System.out.println(arrtemp1[0]);
-        System.out.println(arrtemp1[1]);
-
+        
         boolean express = Arrays.asList(arrtemp1).contains("a");
-        System.out.println(express);
-
-        if (express == true){
+        if (express == true) {
             a1 = arrtemp1[1];
             b1 = arrtemp2[1];
-        } else{
+        } else {
             b1 = arrtemp1[1];
             a1 = arrtemp2[1];
         }
 
-
-        System.out.println(Arrays.asList(arrtemp1).contains("a"));
-        // Arrays.asList(arrtemp1).contains("a");
-
-
-
-        // for (int i = 0; i < arrtemp1.length; i++) {
-        //     if (arrtemp1[0] == "a") {
-        //         System.out.println("a тута");
-        //     } else {
-        //         System.out.println("нет");
-        //     }
-        // }
-
-        // a1 = arrtemp1[1];
-        // b1 = arrtemp2[1];
-
-        System.out.println(a1);
-        // System.out.println(b1);
+        System.out.println("a = " + a1);
+        System.out.println("b = " + b1);
 
         int a = Integer.parseInt(a1);
         int b = Integer.parseInt(b1);
+        finalarr[0] = a;
+        finalarr[1] = b;
 
-        // System.out.println("a = " + a);
-        // System.out.println("b = " + b);
+        return finalarr;
+    }
 
-        System.out.println(a + " в степени " + b + " равен - " + calc(a, b));
-        double c = calc(a, b);
+    public static double calc(int [] arr) {
+        double result = Math.pow(arr[0], arr[1]);
+        return result;
 
+    }
+
+    public static void writeData(double data, int []arr) throws IOException {
+        
         try (final FileWriter writer = new FileWriter("file.txt", false)) {
-            final String s = Double.toString(c);
-            writer.write(s);
+            final String s = Double.toString(data);
+            writer.write(arr[0] + " в степени " + arr[1] + " равен = " + s);
             writer.write(System.lineSeparator());
-            // System.out.println(s);
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static double calc(int a, int b) {
-        double result = Math.pow(a, b);
-        return result;
-
-    }
 
 }
