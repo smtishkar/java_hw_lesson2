57f18125f2dedf1ba8f0cc46b90c6b0a90d91326
Sergey <smtishkar@mail.ru>
2023-01-25 13:12:34 +0300

    2. Считал файл и разбил на 2а массива

--- lesson2.java
+++ lesson2.java
@@ -1,5 +1,10 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 /**
  * lesson2
@@ -7,6 +12,49 @@ import java.io.IOException;
 public class lesson2 {
 
     public static void main(String[] args) throws IOException {
+        String path = "Input.txt";
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        // String line = scanner.nextLine();
+        // String[] words = line.split(" ");
+
+        ArrayList<String> data = new ArrayList<String>() ;
+        while (scanner.hasNextLine()){
+            data.add(scanner.nextLine());
+        }
+
+        System.out.println(data);
+        String[] simpleArray = data.toArray(new String[]{});
+        System.out.println(Arrays.toString(simpleArray));
+        scanner.close();
+
+        String str1 = simpleArray[0];
+        String [] arrtemp1 = str1.split(" = ");
+        for (String ss1: arrtemp1 ){
+            System.out.println(ss1);
+        }
+        System.out.println(Arrays.toString(arrtemp1));
+
+        String str2 = simpleArray[1];
+        String [] arrtemp2 = str2.split(" = ");
+        for (String ss2: arrtemp2 ){
+            System.out.println(ss2);
+        }
+        System.out.println(Arrays.toString(arrtemp1));
+        System.out.println(Arrays.toString(arrtemp2));
+
+
+        // String path = "Input.txt";
+        // File file = new File(path);
+        // Scanner scanner = new Scanner(file);
+        // while (scanner.hasNextLine()){
+        //     System.out.println(scanner.nextLine());
+        // }
+        // scanner.close();
+
+
+
+
         double a = 2;
         double b = 3;
         System.out.println(calc(a, b));
@@ -29,5 +77,7 @@ public class lesson2 {
         return result;
 
     }
+
+    
 
 }
